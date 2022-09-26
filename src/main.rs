9b003c9c3a4f9f2a46f3b1b2ac9b79502e94c25f
tonyi <tony.isupov2010@yandex.ru>
2022-09-26 13:05:24 +0300

    ESP8266. RTIC framework. led control.

--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,124 @@
+#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
-use core::fmt::Write;
-use cortex_m::delay::Delay;
-use cortex_m::singleton;
 use panic_rtt_target as _;
 
-use stm32f1xx_hal::{prelude::*, pac};
-use cortex_m_rt::entry;
-use dht11::Dht11;
-use embedded_hal::blocking::delay;
-use nb::block;
-use stm32f1xx_hal::dma::Half;
-use stm32f1xx_hal::gpio::{CRH, Output, Pin, PushPull};
-use stm32f1xx_hal::pac::USART2;
-use stm32f1xx_hal::serial::{Config, Event, Rx, Rx2, Serial, StopBits, Tx};
-use stm32f1xx_hal::time::{Hertz};
-use stm32f1xx_hal::pac::interrupt;
-use rtt_target::{rtt_init_print, rprintln, rprint};
+#[rtic::app(device = stm32f1xx_hal::pac)]
+mod app {
+    use core::fmt::Write;
+    use cortex_m::prelude::_embedded_hal_serial_Read;
+    use embedded_hal::digital::v2::OutputPin;
 
-const CLOCK_SPEED: u32 = 16 * 1000 * 1000;
+    use rtt_target::{rprint, rprintln, rtt_init_print};
+    use stm32f1xx_hal::afio::AfioExt;
+    use stm32f1xx_hal::flash::FlashExt;
+    use stm32f1xx_hal::gpio::{CRH, GpioExt, Output, Pin, PushPull};
+    use stm32f1xx_hal::pac::{Interrupt, USART2};
+    use stm32f1xx_hal::rcc::RccExt;
+    use stm32f1xx_hal::serial::{Config, Rx, Serial, StopBits};
+    use stm32f1xx_hal::time::{Hertz, U32Ext};
+    use heapless::spsc::{Consumer, Producer, Queue};
 
-static mut RX: Option<Rx<USART2>> = None;
-static mut TX: Option<Tx<USART2>> = None;
-static mut LED: Option<Pin<Output<PushPull>, CRH, 'C', 13>> = None;
-
-// Определяем входную функцию.
-#[entry]
-fn main() -> ! {
-    rtt_init_print!();
-    // Получаем управление над аппаратными средствами
-    let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = pac::Peripherals::take().unwrap();
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
-
-    let mut afio = dp.AFIO.constrain();
-    let mut gpioc = dp.GPIOC.split();
-    let mut gpioa = dp.GPIOA.split();
-
-    let tx = gpioa.pa2.into_alternate_push_pull(&mut gpioa.crl);
-    let rx = gpioa.pa3;
-
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-    led.set_high();
-
-    let clocks = rcc.cfgr.sysclk(Hertz::from_raw(CLOCK_SPEED)).freeze(&mut flash.acr);
-    let mut delay = Delay::new(cp.SYST, CLOCK_SPEED);
-    let mut serial = Serial::usart2(
-        dp.USART2,
-        (tx, rx),
-        &mut afio.mapr,
-        Config::default()
-            .baudrate(115200.bps())
-            .stopbits(StopBits::STOP1)
-            .parity_none(),
-        clocks,
-    );
-    let (mut tx1, mut rx1) = serial.split();
-    tx1.listen();
-    rx1.listen();
-    rx1.listen_idle();
-
-    cortex_m::interrupt::free(|_| unsafe {
-        TX.replace(tx1);
-        RX.replace(rx1);
-        LED.replace(led);
-    });
-
-    unsafe {
-        cortex_m::peripheral::NVIC::unmask(pac::Interrupt::USART2);
+    #[shared]
+    struct Shared {
+        p: Producer<'static, u32, 5>,
+        c: Consumer<'static, u32, 5>,
+        rx1: Option<Rx<USART2>>,
+        led: Option<Pin<Output<PushPull>, CRH, 'C', 13>>
     }
 
-    unsafe {
-        write("AT+CIPMUX=1\r\n".as_bytes());
-        write("AT+CIPSERVER=1\r\n".as_bytes())
+    #[local]
+    struct Local {}
+
+    #[init(local = [q: Queue<u32, 5> = Queue::new()])]
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        rtt_init_print!();
+        // Cortex-M peripherals
+        let _core: cortex_m::Peripherals = cx.core;
+
+        // Device specific peripherals
+        let _device: stm32f1xx_hal::pac::Peripherals = cx.device;
+
+        let mut flash = _device.FLASH.constrain();
+        let rcc = _device.RCC.constrain();
+
+        let mut afio = _device.AFIO.constrain();
+        let mut gpioc = _device.GPIOC.split();
+        let mut gpioa = _device.GPIOA.split();
+
+        let tx = gpioa.pa2.into_alternate_push_pull(&mut gpioa.crl);
+        let rx = gpioa.pa3;
+
+        let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+        led.set_high();
+
+        let clocks = rcc.cfgr.sysclk(Hertz::from_raw( 16 * 1000 * 1000)).freeze(&mut flash.acr);
+
+        let serial = Serial::usart2(
+            _device.USART2,
+            (tx, rx),
+            &mut afio.mapr,
+            Config::default()
+                .baudrate(115200.bps())
+                .stopbits(StopBits::STOP1)
+                .parity_none(),
+            clocks,
+        );
+        let (mut tx1, mut rx1) = serial.split();
+
+        tx1.listen();
+        rx1.listen();
+        rx1.listen_idle();
+
+        tx1.write_str("AT+CIPMUX=1\r\n").unwrap();
+        tx1.write_str("AT+CIPSERVER=1\r\n").unwrap();
+
+        let (p, c) = cx.local.q.split();
+
+        rtic::pend(Interrupt::USART2);
+
+        rprintln!("init");
+
+        (Shared { p, c, rx1: (Option::Some(rx1)), led: (Option::Some(led)) }, Local {}, init::Monotonics())
     }
 
-    loop {
-        cortex_m::asm::wfi()
+    #[idle(shared = [c])]
+    fn idle(_: idle::Context) -> ! {
+        loop {
+            cortex_m::asm::nop();
+        }
     }
-}
 
-const BUFFER_LEN: usize = 4096;
-static mut BUFFER: &mut [u8; BUFFER_LEN] = &mut [0; BUFFER_LEN];
-static mut WIDX: usize = 0;
-
-unsafe fn write(buf: &[u8]) {
-    if let Some(tx) = TX.as_mut() {
-        buf.iter()
-            .for_each(|w| if let Err(_err) = nb::block!(tx.write(*w)) {})
-    }
-}
-
-#[interrupt]
-unsafe fn USART2() {
-    cortex_m::interrupt::free(|_| {
-        if let Some(led) = LED.as_mut() {
-            if let Some(rx) = RX.as_mut() {
+    #[task(binds = USART2, shared = [p, rx1, led])]
+    fn usart2_interrupt(mut cx: usart2_interrupt::Context) {
+        cx.shared.rx1.lock(|rx: _| {
+            if let Some(rx) = rx.as_mut() {
                 if rx.is_rx_not_empty() {
                     if let Ok(w) = nb::block!(rx.read()) {
-                        BUFFER[WIDX] = w;
-                        WIDX += 1;
-                    }
-                } else {
-                    for (i, b) in (&mut BUFFER[0..WIDX]).iter().enumerate() {
-                        let byte = *b as char;
+                        let byte = w as char;
                         rprint!("{}", byte);
                         if byte == '&' {
                             rprintln!("LED ON");
-                            led.set_low();
+                            cx.shared.led.lock(|led1: _| {
+                                if let Some(l) = led1.as_mut() {
+                                    l.set_low();
+                                }
+                            })
                         }
                         if byte == '#' {
                             rprintln!("LED OFF");
-                            led.set_high();
+                            cx.shared.led.lock(|led1: _| {
+                                if let Some(l) = led1.as_mut() {
+                                    l.set_high();
+                                }
+                            })
                         }
                     }
-
-                    WIDX = 0;
                 }
             }
-        }
-    })
+        })
+
+    }
+
 }
